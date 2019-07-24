25f9751e3fdcc2f7a604db50ba732a58c9eaa7e6
Rajesh v <rajeshv@QMAC0313.local>
2019-07-24 11:50:45 +0530

    added msite setup

--- src/test/java/TestObject/TestBase.java
+++ src/test/java/TestObject/TestBase.java
@@ -1,5 +1,9 @@
 package TestObject;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -8,6 +12,8 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -18,8 +24,11 @@ import java.util.concurrent.TimeUnit;
 public class TestBase {
     private Properties config;
     public Properties testConfig;
-    private String browser;
+    private String browser,platform;
     public RemoteWebDriver driver;
+    public AppiumDriver appiumDriver;
+    String androidAppPath=null;
+    String androidAppName=null;
 
     @BeforeClass(alwaysRun = true)
     public void initObject(){
@@ -57,6 +66,7 @@ public class TestBase {
 
     private void selectBrowser(){
         browser=config.getProperty("browser");
+        platform=config.getProperty("PLATFROM");
         if (browser.equalsIgnoreCase("chrome")){
             WebDriverManager.chromedriver().setup();
         }else if (browser.equalsIgnoreCase("firefox")){
@@ -64,19 +74,38 @@ public class TestBase {
         }
     }
 
-    private RemoteWebDriver initiateDriver(){
+    private void initiateDriver(){
         DesiredCapabilities capabilities=null;
+
         if (browser.equalsIgnoreCase("chrome")){
             capabilities=DesiredCapabilities.chrome();
         }else if (browser.equalsIgnoreCase("firefox")){
             capabilities=DesiredCapabilities.firefox();
+        }else if (platform.equalsIgnoreCase("ANDROID")){
+            File appDir = new File(androidAppPath);
+            File app = new File(appDir, androidAppName);
+            capabilities=new DesiredCapabilities();
+            capabilities.setCapability("deviceName", "ZY223RG6L3");
+            capabilities.setCapability("platformName", "Android");
+            capabilities.setCapability("unicodeKeyboard", "true");
+            capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"uiautomator2");
+            capabilities.setCapability("newCommandTimeout", "45000");
+            capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+            capabilities.setCapability("app", app.getPath());
+            capabilities.setCapability("appPackage", "com.quikr");
+            capabilities.setCapability("appActivity", "com.quikr.old.SplashActivity");
+            capabilities.setCapability(AndroidMobileCapabilityType.AUTO_GRANT_PERMISSIONS, true);
+            try {
+                appiumDriver=new AndroidDriver(new URL(config.getProperty("mURL")),capabilities);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
         }
         try {
             driver = new RemoteWebDriver(new URL(config.getProperty("URL")),capabilities);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        return driver;
     }
 
     @AfterMethod(alwaysRun = true)
